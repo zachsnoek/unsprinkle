0a4f8bcf3346155cdd333dcf15765651a5fd4866
zachsnoek <dev@zachsnoek.com>
2022-10-05 19:48:27 -0700

    complete exercise 4

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -52,17 +52,23 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 8px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    padding: 4px 0px;
 `;
 
 const Tag = styled.li`
+    display: inline;
     padding: 4px 8px;
     background: var(--color-gray-300);
     font-size: 0.875rem;
     font-weight: 475;
     color: var(--color-gray-800);
+
+    &:not(:last-of-type) {
+        margin-right: 8px;
+    }
 `;
 
 export default PhotoGridItem;
